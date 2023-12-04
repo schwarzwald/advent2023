f3b24e5d5460a8bcb186c017235e8dba0bba8a0e
bibo <bibaso@gmail.com>
2023-12-04 21:23:08 +0100

    Day 4 improvement

--- day04/part2.js
+++ day04/part2.js
@@ -2,21 +2,21 @@ module.exports = input => {
   let cards = input.split(/\r?\n/)
     .map(r => r.split(': ')[1].split(' | '))
     .map(([w, c]) => [
-      w.split(/\s+/).filter(s => s.trim() != '').map(Number),
-      c.split(/\s+/).filter(s => s.trim() != '').map(Number)
+      w.split(' ').filter(s => s != '').map(Number),
+      c.split(' ').filter(s => s != '').map(Number)
     ])
     .map(([w, c]) => c.filter(x => w.includes(x)).length);
 
-  let history = [];
+  let history = new Array(cards.length);
   let total = 0;
 
   for (let i = cards.length - 1; i >= 0; i--) {
     let wins = cards[i];
     let calls = 1;
-    for (let j = 0; j < wins; j++) {
-      calls += history[cards.length - i - j - 2];
+    for (let j = 1; j <= wins; j++) {
+      calls += history[i + j];
     }
-    history.push(calls);
+    history[i] = calls;
     total += calls;
   }
 
