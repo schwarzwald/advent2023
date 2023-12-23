8bbcd3b7baf55bf18229920068fa5de68b71fc2a
bibo <bibaso@gmail.com>
2023-12-23 02:14:00 +0100

    Day 21

--- day21/test.js
+++ day21/test.js
@@ -1,6 +1,5 @@
 const expect = require('expect.js');
 const part1 = require('./part1');
-const part2 = require('./part2');
 
 describe('Day 21: Part 1', () => {
   it('Should count on how many places can be reached in 6 steps', () => {
@@ -15,20 +14,5 @@ describe('Day 21: Part 1', () => {
 .##.#.####.
 .##..##.##.
 ...........`, 6)).to.equal(16);
-  });
-});
-describe('Day 21: Part 2', () => {
-  it('Should count on how many places can be reached in 6 steps', () => {
-    expect(part2(`...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........`, 100)).to.equal(16);
   });
 });
